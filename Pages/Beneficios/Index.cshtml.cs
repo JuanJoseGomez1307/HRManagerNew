c80008fb586acd273da8200431dc84a6ade1ecde
DfToroAnaya <dtoroanaya16@gmail.com>
2024-05-20 22:23:15 -0500

    Razor Page Index-Beneficios

---
+++ Pages/Beneficios/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using HRManagerNew.Data;
+using HRManagerNew.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRManagerNew.Pages.Beneficios
+{
+    public class IndexModel : PageModel
+    {
+        private readonly HRManagerNewContext _context;
+
+        public IndexModel(HRManagerNewContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Beneficio> Beneficios { get; set; } = default!;
+        public async Task OnGetAsync()
+        {
+            if (_context.Beneficios != null)
+            {
+                Beneficios = await _context.Beneficios.ToListAsync();
+            }
+        }
+    }
+}
